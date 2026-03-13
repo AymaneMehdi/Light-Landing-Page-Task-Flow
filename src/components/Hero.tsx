bb595b02cb13ef12bca1566374b0139808e67f22
Aymane Mehdi <yorubornin1999@gmail.com>
2026-03-13 15:13:01 +0000

    Enhance Hero and Testimonials components with animations, responsive design, and improved structure

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,43 +1,58 @@
 "use client";
-import cylinederImage from "@/assets/cylinder.png";
 import ArrowIcon from "@/assets/arrow-right.svg";
 import cogImage from "@/assets/cog.png";
+import cylinderImage from "@/assets/cylinder.png";
 import noodleImage from "@/assets/noodle.png";
-import { motion, useScroll, useTransform } from "framer-motion";
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useMotionValueEvent,
+} from "framer-motion";
+import Image from "next/image";
 import { useRef } from "react";
+
 export const Hero = () => {
-  const heroRef = useRef<HTMLElement>(null);
+  const heroRef = useRef(null);
   const { scrollYProgress } = useScroll({
     target: heroRef,
     offset: ["start end", "end start"],
   });
+
   const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+    <section
+      ref={heroRef}
+      className="pt-8 pb-20 md:pt-5 md:pb-10 overflow-x-clip"
+      style={{
+        background:
+          "radial-gradient(ellipse 200% 100% at bottom left, #183EC2, #EAEEFE 100%)",
+      }}
+    >
       <div className="container">
         <div className="md:flex items-center">
           <div className="md:w-[478px]">
             <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
               Version 1.0 is here
             </div>
-            <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text [-webkit-background-clip:text] [-webkit-text-fill-color:transparent] mt-6">
+            <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
               All in One Dashboard
             </h1>
             <p className="text-xl text-[#010D3E] tracking-tight mt-6">
               Manage projects, assign tasks, and monitor progress from a single
               powerful workspace designed for modern teams.
             </p>
-            <div className="flex gap-2 items-center mt-[30px]">
+            <div className="flex gap-1 items-center mt-[30px]">
               <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">
                 Get started for free
               </button>
               <button className="bg-white text-black px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight gap-1">
                 <span>Learn more</span>
-                <ArrowIcon className="h-5 w-5" />
               </button>
             </div>
           </div>
-          <div className="mt-20 md:mt-0 md:h-[668px] md:flex-1 relative">
+          <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
             <motion.img
               src={cogImage.src}
               alt="Cog"
@@ -48,25 +63,29 @@ export const Hero = () => {
               transition={{
                 repeat: Infinity,
                 repeatType: "mirror",
-                duration: 2,
+                duration: 3,
                 ease: "easeInOut",
               }}
             />
             <motion.img
-              src={cylinederImage.src}
+              src={cylinderImage.src}
               width={220}
               height={220}
-              alt="Cylinder"
+              alt="Cylinder image"
               className="hidden md:block -top-8 -left-32 md:absolute"
-              style={{ translateY: translateY }}
+              style={{
+                translateY: translateY,
+              }}
             />
             <motion.img
               src={noodleImage.src}
               width={220}
-              height={220}
-              alt="Noodle"
-              className="absolute top-[524px] left-[448px] hidden lg:block rotate-[30deg]"
-              style={{ translateY: translateY, rotate: 30 }}
+              alt="Noodle image"
+              className="hidden lg:block top-[524px] left-[448px] absolute rotate-[30deg]"
+              style={{
+                rotate: 30,
+                translateY: translateY,
+              }}
             />
           </div>
         </div>
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,3 +1,4 @@
+"use client";
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
@@ -7,6 +8,10 @@ import avatar6 from "@/assets/avatar-6.png";
 import avatar7 from "@/assets/avatar-7.png";
 import avatar8 from "@/assets/avatar-8.png";
 import avatar9 from "@/assets/avatar-9.png";
+import Image from "next/image";
+import React, { use } from "react";
+import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
 
 const testimonials = [
   {
@@ -65,6 +70,98 @@ const testimonials = [
   },
 ];
 
+const firstColumn = testimonials.slice(0, 3);
+const secondColumn = testimonials.slice(3, 6);
+const thirdColumn = testimonials.slice(6, 9);
+
+const TestimonialsColumn = (props: {
+  className?: string;
+  testimonials: typeof testimonials;
+  duration?: number;
+}) => {
+  return (
+    <div className={props.className}>
+      <motion.div
+        animate={{
+          translateY: "-50%",
+        }}
+        transition={{
+          duration: props.duration || 10,
+          repeat: Infinity,
+          ease: "linear",
+          repeatType: "loop",
+        }}
+        className="flex flex-col gap-6 pb-6"
+      >
+        {[
+          ...new Array(2).fill(0).map((_, index) => (
+            <React.Fragment key={index}>
+              {props.testimonials.map(({ text, imageSrc, name, username }) => (
+                <div
+                  className="p-10 border rounded-3xl shadow-[0_7px_20px_rgba(0,0,0,0.12)] max-w-sm w-full"
+                  key={username}
+                >
+                  <div>{text}</div>
+                  <div className="flex items-center gap-2 mt-5">
+                    <Image
+                      width={40}
+                      height={40}
+                      src={imageSrc}
+                      alt={name}
+                      className="h-10 w-10 rounded-full"
+                    />
+                    <div className="flex flex-col">
+                      <div className="font-medium tracking-tight leading-5">
+                        {name}
+                      </div>
+                      <div className="leading-5 tracking-tight">{username}</div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </React.Fragment>
+          )),
+        ]}
+      </motion.div>
+    </div>
+  );
+};
+
 export const Testimonials = () => {
-  return null;
+  return (
+    <section className="bg-white py-24 -mb-24">
+      <div className="container">
+        <div className="max-w-[540px] mx-auto">
+          <div className="flex justify-center">
+            <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
+              Testimonials
+            </div>
+          </div>
+
+          <h2 className="text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5 md:text-[54px] md:leading-[60px]">
+            What our users say
+          </h2>
+          <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+            Effortlessly turn your ideas into a fully functional, responsive,
+            no-code SaaS website in just minutes with the set of free components
+            for Framer.
+          </p>
+        </div>
+
+        <div className="flex justify-center gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)] max-h-[740px] overflow-hidden">
+          <TestimonialsColumn testimonials={firstColumn} duration={15} />
+          <TestimonialsColumn
+            testimonials={secondColumn}
+            className="hidden md:flex"
+            duration={19}
+          />
+          <TestimonialsColumn
+            testimonials={thirdColumn}
+            className="hidden lg:flex"
+            duration={17}
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
